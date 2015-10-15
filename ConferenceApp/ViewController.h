15205e1adf91fb3b9be5d18068b3e40f6d7ed528
Students <Students@Rouskas.local>
2015-10-14 23:14:04 -0400

    Initial Commit

---
+++ ConferenceApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConferenceApp
+//
+//  Created by Students on 10/14/15.
+//  Copyright (c) 2015 Students. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
