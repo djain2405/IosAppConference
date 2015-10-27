a35bf4490b3e9f9f21d76bd6bef745ca91a4a4f8
Students <Students@Rouskas.local>
2015-10-27 00:10:58 -0400

    Adding changes for settings form

--- ConferenceApp/Settings.m
+++ ConferenceApp/Settings.m
@@ -7,7 +7,32 @@
 //
 
 #import "Settings.h"
+@interface Settings ()
+@property (weak, nonatomic) IBOutlet UITextField *TitleField;
 
+@end
 @implementation Settings
+
+- (IBAction)OnSave:(id)sender {
+    
+    NSString *titleString = _TitleField.text;
+    
+    PFObject* obj = [PFObject objectWithClassName:@"Conference"];
+    obj[@"ConferenceName"] = titleString;
+    [obj saveInBackgroundWithBlock:^(BOOL succeeded , NSError *error){
+        if(succeeded == YES)
+        {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success" message:@"Your Title has been saved!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+            [alert show];
+        }
+        else
+        {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Your Title has not been saved!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+            [alert show];
+        }
+        
+    }];
+    
+}
 
 @end
