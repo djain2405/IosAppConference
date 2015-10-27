e6986d351c1f96ae673e46eaee187e72ba54c54c
Students <Students@Rouskas.local>
2015-10-27 00:08:02 -0400

    Changes for uploading to parse

---
+++ ConferenceApp/Settings.h
@@ -0,0 +1,14 @@
+//
+//  Settings.h
+//  ConferenceApp
+//
+//  Created by Students on 10/26/15.
+//  Copyright (c) 2015 Students. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface Settings : UIViewController
+
+@end
---
+++ ConferenceApp/Settings.m
@@ -0,0 +1,13 @@
+//
+//  Settings.m
+//  ConferenceApp
+//
+//  Created by Students on 10/26/15.
+//  Copyright (c) 2015 Students. All rights reserved.
+//
+
+#import "Settings.h"
+
+@implementation Settings
+
+@end
